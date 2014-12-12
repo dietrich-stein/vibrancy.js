918a1cbb9678b2b5ac51eaa8301879ef32e40513
Dietrich Stein <dietrich.stein@gmail.com>
2014-12-12 03:15:20 -0500

    Updated generated pages for live dmeo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,30 @@
-console.log('This would be the main JS file.');
+$(function() {
+
+    var t1Vibrancy = $('.target-1').vibrancy({
+        panelClass: 'panel',
+        backgroundSrc: $('.thumbs-1 img:first-child').attr('src')
+    }).data('vibrancy');
+
+    // Thumbnails
+    $('.thumbs-1 img').on('mousedown', function(e) {
+
+        var $img = $(this);
+        if ($img.hasClass('active')) {
+            return;
+        }
+
+        $('.thumbs-1 img').removeClass('active');
+        $(this).addClass('active');
+
+        var numVibs = t1Vibrancy.vibObjects.length;
+        $.each(t1Vibrancy.vibObjects, function(i, o) {
+            o.canvas.stop(true, false).fadeOut(100, function() {
+                numVibs--;
+                if (numVibs === 0) {
+                    t1Vibrancy.loadBackground($img.attr('src'));
+                }
+            });
+        });
+    });
+
+});
